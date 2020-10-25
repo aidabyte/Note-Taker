933a495bac7b28d66402230ac3bb91ebd43923e4
aidabyte <ablinstrubyte@gmail.com>
2020-10-25 15:11:57 -0500

    added static

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,13 @@
 var express = require("express");
 var path = require("path")
 
+const userNotes = [
+    {
+        name: "aida",
+        notes: "hello"
+
+}]
+
 
 
 var PORT = process.env.PORT || 3000;
@@ -10,15 +17,23 @@ var app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static("public"));
+
 app.get("/", function(req, res) {
     // concatanating where my file is
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.get("/Notes", function(req, res) {
+app.get("/notes", function(req, res) {
     // concatanating where my file is
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
+
+app.get("/api/notes",function(req,res) {
+    res.json(userNotes)
+})
+
+
 
 
 
